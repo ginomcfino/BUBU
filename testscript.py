8bb573e597b01a86e6901ce75f12e1741b1a81ca
ginomcfino <jiweiqi717@gmail.com>
2022-08-25 07:37:55 -0400

    making classes to for robot object

--- testscript.py
+++ testscript.py
@@ -9,4 +9,15 @@ servo_positions = {'flf':14, 'fll':12, 'fls':9,
                    'rrs':4, 'rrl':7, 'rrf':0,
                    'frs':8, 'frl':11, 'frf':15}
 
-servoPIN = 
+# with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
+#     for sName in servo_names:
+#         # executor.map(move_servo_angle, (sName, 90,))
+#         move_servo_angle(sName, 90)
+
+# threads = []
+# for sn in servo_names:
+#     t = threading.Thread(target=set_servo_angle, args=(sn, 90))
+#     threads.append(t)
+#     t.start()
+# for t in threads:
+#     t.join()
